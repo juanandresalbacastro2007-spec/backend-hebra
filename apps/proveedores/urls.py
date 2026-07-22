252ace8876fee20c2d147d81b0e61c276e359863
Jorge <jorgeformulaone@gmail.com>
2026-07-22 10:32:20 -0500

    Ruta corregida en admin

--- apps/proveedores/urls.py
+++ apps/proveedores/urls.py
@@ -1,8 +1,10 @@
+# apps/proveedores/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_proveedores, name='proveedores'),
+    path('', views.listar_proveedores, name='admin_proveedores'),
     path('crear/', views.crear_proveedor, name='crear_proveedor'),
     path('editar/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
     path('eliminar/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor'),
